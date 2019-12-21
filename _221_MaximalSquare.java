2149e8f6034166e6be4774a7b29946032bc03cc5
mengchaoli <lmcwesdxc@163.com>
2019-12-20 18:46:42 -0800

    12.20

---
+++ _221_MaximalSquare.java
@@ -0,0 +1,24 @@
+//dp[i][j] represents the length of the square which lower right corner is located at (i, j).
+//If the value of this cell is also 1, then the length of the square is the minimum of: the one above, its left, and diagonal up-left value +1.
+// Because if one side is short or missing, it will not form a square.
+
+public class _221_MaximalSquare {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return 0;
+
+        int m = matrix.length, n = matrix[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+        int maxEdge = 0;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]);
+                    maxEdge = Math.max(maxEdge, dp[i][j]);
+                }
+            }
+        }
+        return maxEdge * maxEdge;
+    }
+}
