7fccf1b0197efb8900645fc44bbea5f35aed297c
mengchaoli <lmcwesdxc@163.com>
2020-01-05 23:13:02 -0800

    1.5

---
+++ String/_6_ZigZagConversion.java
@@ -0,0 +1,31 @@
+package String;
+
+public class _6_ZigZagConversion {
+    public String convert(String s, int numRows) {
+        char[] c = s.toCharArray();
+        int len = c.length;
+        StringBuilder[] sb = new StringBuilder[numRows];
+
+        for (int i = 0; i < numRows; i++) {
+            sb[i] = new StringBuilder();
+        }
+
+        int i = 0;
+
+        while (i < len) {
+            for (int idx = 0; idx < numRows && i < len; idx++) {
+                sb[idx].append(c[i++]);
+            }
+
+            for (int idx = numRows - 2; idx >= 1 && i < len; idx--) {
+                sb[idx].append(c[i++]);
+            }
+        }
+
+        for (int i = 1; i < numRows; i++) {
+            sb[0].append(sb[i]);
+        }
+
+        return sb[0].toString();
+    }
+}
