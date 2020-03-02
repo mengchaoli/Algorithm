cd7ac6a0ef11cef9f18b23308767d0e88865c555
mengchaoli <lmcwesdxc@163.com>
2020-03-01 22:59:07 -0800

    fix

--- _554_Brick_Wall.java
+++ _554_Brick_Wall.java
@@ -5,16 +5,16 @@ import java.util.Map;
 public class _554_Brick_Wall {
     public int leastBricks(List<List<Integer>> wall) {
         Map<Integer, Integer> map = new HashMap<>();
-        
+
         int max = 0;
-        
+
         for (List<Integer> row : wall) {
             int pre = 0;
             for (int i = 0; i < row.size() - 1; i++) {
                 pre += row.get(i);
                 int time = map.getOrDefault(pre, 0);
-                
-                
+
+
                 map.put(pre, time + 1);
                 max = Math.max(max, time + 1);
             }
