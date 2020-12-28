3366ef787d6b1432fe10589a02b05b002dd88625
mengchaoli <lmcwesdxc@163.com>
2020-12-27 21:29:22 -0800

    12.27/2020

---
+++ DP/_198_HouseRobber.java
@@ -0,0 +1,15 @@
+package DP;
+
+public class _198_HouseRobber {
+    public int rob(int[] nums) {
+        int prev1 = 0, prev2 = 0;
+
+        for (int num : nums) {
+            int temp = prev1;
+            prev1 = Math.max(prev2 + num, prev1);
+            prev2 = temp;
+        }
+
+        return prev1;
+    }
+}
