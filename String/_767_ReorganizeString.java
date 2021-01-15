ac85e5d7da7c1a822d4a0f1092d67bccea6de1f8
mengchaoli <lmcwesdxc@163.com>
2021-01-14 20:36:17 -0800

    01.15/2021

---
+++ String/_767_ReorganizeString.java
@@ -0,0 +1,44 @@
+package String;
+
+public class _767_ReorganizeString {
+    public String reorganizeString(String S) {
+        int[] count = new int[26];
+        int max = 0, letter = 0;
+
+        for (char c : S.toCharArray()) {
+            count[c - 'a']++;
+            if (count[c - 'a'] > max) {
+                max = count[c - 'a'];
+                letter = c - 'a';
+            }
+        }
+
+        if (max > (S.length() + 1) / 2) {
+            return "";
+        }
+
+        // Actually, only the most occurrence letter matters. That's why we dont need to sort the count array, we just need to settle down the most occurrence letter first
+        int idx = 0;
+        char[] res = new char[S.length()];
+        for (int i = 0; i < max; i++) {
+            res[idx] = (char) (letter + 'a');
+            count[letter]--;
+            idx += 2;
+        }
+
+        // Then settle down the rest
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                if (idx >= S.length()) {
+                    idx = 1;
+                }
+
+                res[idx] = (char) (i + 'a');
+                count[i]--;
+                idx += 2;
+            }
+        }
+
+        return String.valueOf(res);
+    }
+}
