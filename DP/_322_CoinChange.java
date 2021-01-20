e18d69ffeda9443b44416b96e48b3d33ea1f41a3
mengchaoli <lmcwesdxc@163.com>
2021-01-19 23:45:57 -0800

    01.19/2021

--- DP/_322_CoinChange.java
+++ DP/_322_CoinChange.java
@@ -4,21 +4,52 @@ import java.util.Arrays;
 
 public class _322_CoinChange {
     // bottom up solution, iteration
-    public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount];
-        Arrays.fill(dp, amount + 1);
-        dp[0] = 0;
+//    public int coinChange(int[] coins, int amount) {
+//        int[] dp = new int[amount];
+//        Arrays.fill(dp, amount + 1);
+//        dp[0] = 0;
+//
+//        for (int i = 1; i <= amount; i++) {
+//            for (int coin : coins) {
+//                if (i >= coin) {
+//                    dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+//                }
+//            }
+//        }
+//
+//        return dp[amount] == amount + 1 ? -1 : dp[amount];
+//    }
 
-        for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (i >= coin) {
-                    dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
-                }
+    // top down solution, recursion
+    public int coinChange(int[] coins, int amount) {
+        if (amount < 1) {
+            return 0;
+        }
+
+        return coinChange(coins, amount, new int[amount]);
+    }
+
+    private int coinChange(int[] coins, int remain, int[] count) {
+        if (remain < 0) {
+            return -1; // invalid, and we will pass this in below's if condition in line 46
+        }
+        if (remain == 0) {
+            return 0;
+        }
+        if (count[remain - 1] != 0) {
+            return count[remain - 1];
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int res = coinChange(coins, remain - coin, count);
+            if (res >= 0 && res < min) {
+                min = res + 1;
             }
         }
 
-        return dp[amount] == amount + 1 ? -1 : dp[amount];
-    }
+        count[remain - 1] = (min == Integer.MAX_VALUE ? -1 : min);
 
-    // top down solution, recursion
+        return count[remain - 1];
+    }
 }
