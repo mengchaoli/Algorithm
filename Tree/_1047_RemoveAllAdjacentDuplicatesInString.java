b4a2e23e120acec3ffbcc24085de941a945e8fe3
mengchaoli <lmcwesdxc@163.com>
2021-01-21 22:40:23 -0800

    01.21/2021

--- Tree/_1047_RemoveAllAdjacentDuplicatesInString.java
+++ Tree/_1047_RemoveAllAdjacentDuplicatesInString.java
@@ -2,16 +2,29 @@ package Tree;
 
 public class _1047_RemoveAllAdjacentDuplicatesInString {
     public String removeDuplicates(String S) {
-        StringBuilder sb = new StringBuilder();
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (char c : S.toCharArray()) {
+//            if (sb.length() != 0 && c == sb.charAt(sb.length() - 1)) {
+//                sb.deleteCharAt(sb.length() - 1);
+//            } else {
+//                sb.append(c);
+//            }
+//        }
+//
+//        return sb.toString();
 
-        for (char c : S.toCharArray()) {
-            if (sb.length() != 0 && c == sb.charAt(sb.length() - 1)) {
-                sb.deleteCharAt(sb.length() - 1);
-            } else {
-                sb.append(c);
+        // Solution 2:
+        char[] res = S.toCharArray();
+        int i = 0;
+
+        for (int j = 0; j < S.length(); j++, i++) {
+            res[i] = res[j];
+            if (i > 0 && res[i] == res[i - 1]) { // this means we found duplicate of 2 same characters
+                i -= 2;
             }
         }
 
-        return sb.toString();
+        return new String(res, 0, i);
     }
 }
--- Tree/_1209_RemoveAllAdjacentDuplicatesinStringII.java
+++ Tree/_1209_RemoveAllAdjacentDuplicatesinStringII.java
@@ -4,31 +4,46 @@ import java.util.Stack;
 
 public class _1209_RemoveAllAdjacentDuplicatesinStringII {
     public String removeDuplicates(String s, int k) {
-        Stack<Character> stack = new Stack<>();
-        Stack<Integer> count = new Stack<>();
+//        Stack<Character> stack = new Stack<>();
+//        Stack<Integer> count = new Stack<>();
+//
+//        for (char c : s.toCharArray()) {
+//            if (stack.isEmpty() || c != stack.peek()) {
+//                stack.push(c);
+//                count.push(1);
+//            } else {
+//                if (count.peek() == k - 1) {
+//                    for (int i = 0; i < k - 1; i++) {
+//                        stack.pop();
+//                    }
+//                    count.pop();
+//                } else {
+//                    stack.push(c);
+//                    count.push(count.pop() + 1);
+//                }
+//            }
+//        }
+//
+//        StringBuilder sb = new StringBuilder();
+//        while (!stack.isEmpty()) {
+//            sb.insert(0, stack.pop());
+//        }
+//
+//        return sb.toString();
 
-        for (char c : s.toCharArray()) {
-            if (stack.isEmpty() || c != stack.peek()) {
-                stack.push(c);
-                count.push(1);
-            } else {
-                if (count.peek() == k - 1) {
-                    for (int i = 0; i < k - 1; i++) {
-                        stack.pop();
-                    }
-                    count.pop();
-                } else {
-                    stack.push(c);
-                    count.push(count.pop() + 1);
-                }
+        // Solution 2:
+        int[] count = new int[s.length()];
+        int i = 0;
+        char[] res = s.toCharArray();
+
+        for (int j = 0; j < s.length(); j++, i++) {
+            res[i] = res[j];
+            count[i] = i > 0 && res[i] == res[i - 1] ? count[i - 1] + 1 : 1;
+            if (count[i] == k) {
+                i -= k;
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.insert(0, stack.pop());
-        }
-
-        return sb.toString();
+        return new String(res, 0, i);
     }
 }
