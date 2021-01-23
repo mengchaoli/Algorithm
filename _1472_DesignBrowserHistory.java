830d7a83c72ea3c33b044c0e28b42829c2af450b
mengchaoli <lmcwesdxc@163.com>
2021-01-22 23:36:13 -0800

    01.22/2021

---
+++ _1472_DesignBrowserHistory.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class _1472_DesignBrowserHistory {
+
+    Stack<String> inStack = new Stack<>();
+    Stack<String> outStack = new Stack<>();
+
+    public _1472_DesignBrowserHistory(String homepage) {
+        inStack.push(homepage);
+    }
+
+    public void visit(String url) {
+        inStack.push(url);
+        outStack.clear();
+    }
+
+    public String back(int steps) {
+        int min = Math.min(steps, inStack.size() - 1);
+        for (int i = 0; i < min; i++) {
+            outStack.push(inStack.pop());
+        }
+
+        return inStack.peek();
+    }
+    public String forward(int steps) {
+        int min = Math.min(steps, outStack.size());
+        for (int i = 0; i < min; i++) {
+            inStack.push(outStack.pop());
+        }
+
+        return inStack.peek();
+    }
+}
