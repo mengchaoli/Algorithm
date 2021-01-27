6efd44f602ab82f181b75112590f4e77e1b491f0
mengchaoli <lmcwesdxc@163.com>
2021-01-26 21:42:30 -0800

    01.26/2021 redo 435

--- _435_NonoverlappingIntervals.java
+++ _435_NonoverlappingIntervals.java
@@ -32,11 +32,11 @@ public class _435_NonoverlappingIntervals {
 //
 //        for (int i = 1; i < intervals.length; i++) {
 //            if (intervals[i][0] < prev[1]) { // this means we got overlap, coz current start time is less than previous end time
+//                drop++;
 //                // the only thing we need to pay attention to in this solution is: if intervals[i][1] < prev[1], this means current one is fully included by prev one, so we keep the earlier end time to save room for later intervals
 //                if (intervals[i][1] < prev[1]) {
 //                    prev = intervals[i];
 //                }
-//                drop++;
 //            } else {
 //                prev = intervals[i];
 //            }
