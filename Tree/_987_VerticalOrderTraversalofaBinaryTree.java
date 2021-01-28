e92775f893895e8cef1d41f34853f39ac3eff692
mengchaoli <lmcwesdxc@163.com>
2021-01-27 16:55:04 -0800

    01.27/2020 redo 987

--- Tree/_987_VerticalOrderTraversalofaBinaryTree.java
+++ Tree/_987_VerticalOrderTraversalofaBinaryTree.java
@@ -6,49 +6,68 @@ import java.util.*;
 
 public class _987_VerticalOrderTraversalofaBinaryTree {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        Map<Integer, List<Pair<TreeNode, Pair<Integer, Integer>>>> hashMap = new HashMap<>();
-        Queue<Pair<TreeNode, Pair<Integer, Integer>>> workingQueue = new LinkedList<>();
-        int min = 0, max = 0;
-        workingQueue.offer(new Pair<TreeNode, Pair<Integer, Integer>>(root, new Pair<Integer, Integer>(0, 0)));
+        List<Pair<TreeNode, Pair<Integer, Integer>>> list = new LinkedList<>();
+        Queue<Pair<TreeNode, Pair<Integer, Integer>>> queue = new LinkedList<>();
+        queue.add(new Pair<>(root, new Pair<>(0, 0)));
 
-        while (!workingQueue.isEmpty()) {
-            Pair<TreeNode, Pair<Integer, Integer>> current = workingQueue.poll();
+        while (!queue.isEmpty()) {
+            int size = queue.size();
 
-            min = Math.min(min, current.getValue().getKey());
-            max = Math.max(max, current.getValue().getKey());
+            for (int i = 0; i < size; i++) {
+                Pair<TreeNode, Pair<Integer, Integer>> current = queue.poll();
+                list.add(current);
 
-            if (!hashMap.containsKey(current.getValue().getKey()))
-                hashMap.put(current.getValue().getKey(), new ArrayList<>());
-            hashMap.get(current.getValue().getKey()).add(current);
-
-            if (current.getKey().left != null) {
-                workingQueue.offer(new Pair<TreeNode, Pair<Integer, Integer>>(current.getKey().left, new Pair<Integer, Integer>(current.getValue().getKey() - 1, current.getValue().getValue() + 1)));
-            }
-
-
-            if (current.getKey().right != null) {
-                workingQueue.offer(new Pair<TreeNode, Pair<Integer, Integer>>(current.getKey().right, new Pair<Integer, Integer>(current.getValue().getKey() + 1, current.getValue().getValue() + 1)));
+                if (current.getKey().left != null) {
+                    queue.add(new Pair<>(current.getKey().left, new Pair<>(current.getValue().getKey() - 1, current.getValue().getValue() + 1)));
+                }
+                if (current.getKey().right != null) {
+                    queue.add(new Pair<>(current.getKey().right, new Pair<>(current.getValue().getKey() + 1, current.getValue().getValue() + 1)));
+                }
             }
         }
 
-        for (int i = min; i <= max; i++) {
-            Collections.sort(hashMap.get(i), new Comparator<Pair<TreeNode, Pair<Integer, Integer>>>(){
-                public int compare(Pair<TreeNode, Pair<Integer, Integer>> left, Pair<TreeNode, Pair<Integer, Integer>> right) {
-                    if (left.getValue().getValue() == right.getValue().getValue())
-                        return left.getKey().val - right.getKey().val;
-                    return 0;
+        Collections.sort(list, (o1, o2) -> (o1.getValue().getKey() - o2.getValue().getKey()));
+
+        int i = 0;
+        List<List<Integer>> res = new LinkedList<>();
+        while (i < list.size()) {
+            int currentX = list.get(i).getValue().getKey();
+
+            List<Pair<TreeNode, Pair<Integer, Integer>>> subList = new LinkedList<>();
+            while (i < list.size() && list.get(i).getValue().getKey() == currentX) {
+                subList.add(list.get(i));
+                i++;
+            }
+
+            Collections.sort(subList, (o1, o2) -> {
+                if (o1.getValue().getValue() != o2.getValue().getValue()) {
+                    return o1.getValue().getValue() - o2.getValue().getValue();
+                } else {
+                    return o1.getKey().val - o2.getKey().val;
                 }
             });
 
-            List<Integer> nodeVals = new ArrayList<>();
-
-            for (int j = 0; j < hashMap.get(i).size(); j++) {
-                nodeVals.add(hashMap.get(i).get(j).getKey().val);
+            List<Integer> subAns = new LinkedList<>();
+            for (int j = 0; j < subList.size(); j++) {
+                subAns.add(subList.get(j).getKey().val);
             }
 
-            res.add(nodeVals);
+            res.add(subAns);
         }
+
         return res;
+    }
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
     }
 }
