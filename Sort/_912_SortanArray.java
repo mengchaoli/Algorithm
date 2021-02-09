6a51ed83197d9c05ba137d74b099f98eefd6a254
mengchaoli <lmcwesdxc@163.com>
2021-02-08 22:56:36 -0800

    02.08/2021

---
+++ Sort/_912_SortanArray.java
@@ -0,0 +1,44 @@
+package Sort;
+
+public class _912_SortanArray {
+    public int[] sortArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return nums;
+        }
+
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void quickSort(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int left = start, right = end;
+        // 1. pivot, pick value, not index
+        int pivot = nums[(start + end) / 2];
+
+        // 2. left <= right, not <
+        while (left <= right) {
+            // 3. nums[left] < pivot, not <=
+            while (left <= right && nums[left] < pivot) {
+                left++;
+            }
+            while (left <= right && nums[right] > pivot) {
+                right--;
+            }
+
+            if (left <= right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        quickSort(nums, start, right);
+        quickSort(nums, left, end);
+    }
+}
