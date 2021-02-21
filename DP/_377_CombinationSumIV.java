56fb808243387962b37e8022756741778c25f21b
mengchaoli <lmcwesdxc@163.com>
2021-02-21 13:22:36 -0800

    02.21/2021 redo 377

--- DP/_377_CombinationSumIV.java
+++ DP/_377_CombinationSumIV.java
@@ -24,27 +24,27 @@ public class _377_CombinationSumIV {
     public int combinationSum4(int[] nums, int target) {
         int[] dp = new int[target + 1];
         Arrays.fill(dp, -1);
+        dp[0] = 1;
         return helper(nums, target, dp);
     }
 
     private int helper(int[] nums, int target, int[] dp) {
         if (target < 0) {
             return 0;
-        }
-
-        if (dp[target] != -1) {
+        } else if (dp[target] != -1) {
             return dp[target];
         } else if (target == 0) {
             return 1;
         }
 
-        int res = 0;
+        int count = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            res += helper(nums, target - nums[i], dp);
-            dp[target] = res;
+            count += helper(nums, target - nums[i], dp);
+            // important:
+            dp[target] = count;
         }
 
-        return res;
+        return count;
     }
 }
