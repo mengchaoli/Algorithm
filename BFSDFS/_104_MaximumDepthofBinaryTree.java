d80fdff9a17845a64b9124089f968ea615bfd191
mengchaoli <lmcwesdxc@163.com>
2021-02-24 22:46:11 -0800

    02.24/2021

---
+++ BFSDFS/_104_MaximumDepthofBinaryTree.java
@@ -0,0 +1,21 @@
+package BFSDFS;
+
+public class _104_MaximumDepthofBinaryTree {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
